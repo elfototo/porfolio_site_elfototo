7fa8676a8ae24e5e038d9c1d7ac3a49db6b92b0c
elfototo <e89617881302@gmail.com>
2025-06-03 15:21:39 +0700

    template netlify

--- app/routes.ts
+++
@@ -1,7 +0,0 @@
-import { type RouteConfig, layout, index } from "@react-router/dev/routes";
-
-export default [
-    layout("routes/layout.tsx", [
-        index("routes/home.tsx"),
-      ])
-] satisfies RouteConfig;
