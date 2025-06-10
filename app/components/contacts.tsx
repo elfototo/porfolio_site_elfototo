24f41614572fdf30c47022bb5274e41c9df7030d
elfototo <e89617881302@gmail.com>
2025-06-10 17:58:08 +0700

    contact block fixed text

--- app/components/contacts.tsx
+++ app/components/contacts.tsx
@@ -21,14 +21,14 @@ export default function Contacts() {
 
         {/* content */}
         <div className="flex flex-col lg:order-2 order-1">
-          <div className="max-w-md mx-auto mb-4 lg:order-1 order-2">
-            <h2 className="text-white text-2xl font-inter-extraBold mb-4">
+          <div className="max-w-md mx-auto lg:order-1 order-2">
+            <h2 className="text-white text-2xl font-inter-extraBold mb-2">
               {t("form.text-title")}
             </h2>
             <p className="text-white text-md">{t("form.text-content")}</p>
           </div>
 
-          <div className="flex flex-col items-center lg:items-start lg:w-[80%] w-full lg:order-2 order-1 lg:mb-0 mb-10">
+          <div className="flex flex-col items-center lg:items-start lg:w-[80%] w-full lg:order-2 order-1 lg:mb-0 mb-10 lg:mt-4 mt-0">
             <div className="bg-gray-300 w-full h-[1px] mb-10 lg:hidden block"></div>
             <SocialContacts />
             <div className="bg-gray-300 w-full h-[1px] mt-10 lg:hidden block"></div>
